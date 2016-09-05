c6f88e7901a15e6f030401b767abba89bbe5e958
Lukas Kubis <contact@lukaskubis.com>
2016-09-06 00:02:41 +0200

    initial commit

---
+++ crayon.py
@@ -0,0 +1,144 @@
+from __future__ import print_function
+"""
+Utilities for displaying text in terminals using styling, colors, indentation etc.
+"""
+
+# System color name constants.
+COLORS = (
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    LIGHT_GRAY,
+    DARK_GRAY,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    WHITE,
+) = range(16)
+
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+
+def printout(*args, **kwargs):
+    """
+    Print function with extra options for formating text in terminals.
+    """
+    color = kwargs.pop('color', {})
+    spacing = kwargs.pop('indent', 0)
+    pref = kwargs.pop('prefix', '')
+    suf = kwargs.pop('suffix', '')
+    style = kwargs.pop('style', {})
+
+    # unwrap text from list of arguments and modify it
+    args = list(args)
+    args[0] = colorize(args[0], **color)
+    args[0] = stylize(args[0], **style)
+    args[0] = prefix(args[0], pref)
+    args[0] += str(suf)
+    args[0] = indent(args[0], spacing)
+    args = tuple(args)
+    print(*args, **kwargs)
+
+
+def colorize(txt, **color):
+    """
+    Print escape codes to set the terminal color.
+
+    fg and bg are indices into the color palette for the foreground and
+    background colors.
+    """
+    if not color:
+        return txt
+    fg = color.pop('fg', None)
+    bg = color.pop('bg', None)
+    setting = ''
+    setting += '\x1b[38;5;{}m'.format(fg) if fg else ''
+    setting += '\x1b[48;5;{}m'.format(bg) if bg else ''
+    return setting + txt + '\x1b[0m'
+
+
+def indent(txt, spacing=4):
+    """
+    Indent given text using custom spacing, default is set to 4.
+    """
+    return prefix(txt, ''.join([' ' for _ in range(spacing)]))
+
+
+def prefix(txt, prefix):
+    """
+    Place a prefix in front of the text.
+    """
+    return str(prefix) + txt
+
+
+def stylize(txt, **style):
+    """
+    Changes style of the text.
+    """
+    if not style:
+        return txt
+    bold = style.pop('bold', None)
+    underline = style.pop('underline', None)
+    setting = ''
+    setting += '\033[1m' if bold else ''
+    setting += '\033[4m' if underline else ''
+    return setting + txt + '\033[0m'
+
+
+def rgb(red, green, blue):
+    """
+    Calculate the palette index of a color in the 6x6x6 color cube.
+
+    The red, green and blue arguments may range from 0 to 5.
+    """
+    for value in (red, green, blue):
+        if value not in range(6):
+            raise ColorError('Value must be within 0-5, was {}.'.format(value))
+    return 16 + (red * 36) + (green * 6) + blue
+
+
+def gray(value):
+    """
+    Calculate the palette index of a color in the grayscale ramp.
+
+    The value argument may range from 0 to 23.
+    """
+    if value not in range(24):
+        raise ColorError('Value must be within 0-23, was {}.'.format(value))
+    return 232 + value
+
+
+class ColorError(Exception):
+    pass
+
+# TODO: implement tests and print a test graphic here
+if __name__ == '__main__':
+    from time import sleep
+    for _ in range(2):
+        for r in range(6):
+            for g in range(6):
+                for b in range(6):
+                    txt = 'rgb{}'.format((r, g, b))
+                    c = {'fg': rgb(r, g, b)}
+                    s = {'bold': True, 'underline': True}
+                    printout(txt, color=c, end=' -> ')
+                    printout(txt + ' stylized', color=c, style=s, end=' -> ')
+                    c = {'bg': rgb(r, g, b)}
+                    printout('            ', color=c)
+                    sleep(0.05)
+
+    # for b in range(24):
+    #     txt = 'This is a gray{:02d} color.'.format((b))
+    #     c = {'fg': gray(b)}
+    #     printout(txt, color=c, end=' -> ')
+    #     c = {'bg': gray(b)}
+    #     printout('            ', color=c)
+    #     sleep(0.1)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='crayon',
+      version='1.0',
+      description='Terminal utilities for styling text',
+      long_description='Simple module packed with utilities for customizing terminal output',
+      url='https://github.com/lukaskubis/crayon',
+      author='Lukas Kubis',
+      author_email='contact@lukaskubis.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Stable',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords='color style terminal text utility',
+      py_modules=['crayon'],
+      zip_safe=False
+      )
